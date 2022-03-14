a7d966952e02362887c1ae240f3979a85c620da5
Gustavo <gust.bersan@gmail.com>
2022-03-13 22:24:11 -0300

    Adição do método Subtração
    Este método irá realizar a subtração dos valores inseridos.

--- basic-calculator/Program.cs
+++ basic-calculator/Program.cs
@@ -25,5 +25,20 @@ namespace basic_calculator
             Console.ReadKey();
         }
 
+        static void Subtracao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Insira o primeiro valor:");
+            decimal valor1 = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira o segundo valor:");
+            decimal valor2 = decimal.Parse(Console.ReadLine());
+
+            decimal result = valor1 - valor2;
+
+            Console.WriteLine($"\nO resultado da subtração é {result}");
+            Console.ReadKey();
+        }
     }
 }
