b000d645dade4449adf6bdc27d26fa35400d0dbd
Gustavo <gust.bersan@gmail.com>
2022-03-13 22:33:12 -0300

    Adição do método Divisão
    Este método irá realizar a divisão dos valores inseridos.

--- basic-calculator/Program.cs
+++ basic-calculator/Program.cs
@@ -40,5 +40,19 @@ namespace basic_calculator
             Console.WriteLine($"\nO resultado da subtração é {result}");
             Console.ReadKey();
         }
+
+        static void Divisao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Insira o primeiro valor:");
+            decimal valor1 = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira o segundo valor:");
+            decimal valor2 = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nO resultado da divisão é " + (valor1 / valor2));
+            Console.ReadKey();
+        }
     }
 }
